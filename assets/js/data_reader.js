c92903128c5e5fc2fdf862ecc982691875dee879
Bingobalazs <103860334+Bingobalazs@users.noreply.github.com>
2023-11-09 13:40:38 +0100

    v1.0 Alap funkció Kész

--- assets/js/data_reader.js
+++ assets/js/data_reader.js
@@ -53,7 +53,7 @@ var osztaly = [
     },
     {
         nev: "Tatár Andor",
-        esely: 6 
+        esely: 10 
     },
     {
         nev: "Deme Ákos",
@@ -95,5 +95,29 @@ var osztaly = [
         nev: "M. Tóth Milán",
         esely: 10 
     },
+
+]
+var tetelek = [
+    "1. A középkori város kialakulása és jellemzői",
+    "2. Károly Róbert és az Anjouk kora",
+    "3. A nagy földrajzi felfedezések és következményei",
+    "4. Az első és második ipari forradalom és következményei",
+    "5. A XVIII. századi magyar társadalom",
+    "6. A magyar polgárosodás társadalmi jellegzetességei és sajátosságai",
+    "7. Életmód és mindennapok a Kádár-korszakban",
+    "8. A középkori magyar királyság megteremtése",
+    "9. A központosított rendi monarchia a Hunyadiak korában",
+    "10. A reformmozgalom kibontakozása",
+    "11. Az Egyesült Nemzetek Szervezete",
+    "12. A Horthy-korszak politikai berendezkedése (Bethlen, Teleki)",
+    "13. A dualizmus kora",
+    "14. A reformáció",
+    "15. Mária Terézia és II. József reformjai",
+    "16. A kommunista Szovjetunió",
+    "17. Az első világháború lezárása",
+    "18. A várháborúk időszaka (1526-1568)",
+    "19. Magyarország hadba sodródásának folyamata",
+    "20. A hidegháború kialakulása",
+
 
 ]
--- assets/js/selector.js
+++ assets/js/selector.js
@@ -4,8 +4,41 @@ var osszesely =0
        osszesely+=element.esely 
     });
 
+var nevek = []
+var felelesek = []
+var tetelnev
+var felelnev
+var tetel=0
+function Sorsol(params) {
+    tetel =document.getElementById("tetel").value
+    if (tetel=="RND") {
+        tetel = Math.floor(Math.random() * tetelek.length)
+        tetelnev = tetelek[tetel] 
+    } else {
+         tetelnev = tetelek[tetel-1]
+    }
+    osztaly.forEach(element => {
+        if (true) {
+            for (let i = 0; i < element.esely; i++) {
+            nevek.push(element.nev)
+            
+            }    
+        }
 
-function sorsol(params) {
-    random = Math.floor(Math.random() * osszesely)
-    
+        
+    });
+    random = Math.floor(Math.random() * nevek.length)
+     felelnev= nevek[random-1]
+    felelesek.push( {nev:felelnev, huzott:tetelnev})
+Kiir()
+
+}
+function Tablazat(params) {
+    felelesek.forEach(element => {
+        
+    });
+}
+function Kiir(params) {
+    document.getElementById("sorsNev").innerHTML= felelnev
+    document.getElementById("sorsTetel").innerHTML= tetelnev
 }
