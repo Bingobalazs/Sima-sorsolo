d08e01fb0c5163c79b7833a224a7fc9fceade330
Bingobalazs <103860334+Bingobalazs@users.noreply.github.com>
2023-11-09 12:34:51 +0100

    Alap
    Fileok létrehozva, html kezdetlegesen elkészítve, osztály tanulóinak nevei rögzítve

---
+++ assets/js/data_reader.js
@@ -0,0 +1,99 @@
+var osztaly = [
+    {
+        nev: "Ádám Norbert",
+        esely: 10 
+    },
+    {
+        nev: "Kovács Csaba",
+        esely: 7 
+    },
+    {
+        nev: "Egyedi Róbert",
+        esely: 7 
+    },
+    {
+        nev: "Kozma Máté",
+        esely: 7 
+    },
+    {
+        nev: "Puzsoma Sándor",
+        esely: 10 
+    },
+    {
+        nev: "Nagy Péter",
+        esely: 7 
+    },
+    {
+        nev: "Monoki Gábor",
+        esely: 10 
+    },
+    {
+        nev: "Nagy Attila",
+        esely: 6 
+    },
+    {
+        nev: "Mészáros István",
+        esely: 10 
+    },
+    {
+        nev: "Bere Nándor",
+        esely: 10 
+    },
+    {
+        nev: "Gál Balázs",
+        esely: 4 
+    },
+    {
+        nev: "Szatmári Ádám",
+        esely: 10 
+    },
+    {
+        nev: "Beke Roland",
+        esely: 10 
+    },
+    {
+        nev: "Tatár Andor",
+        esely: 6 
+    },
+    {
+        nev: "Deme Ákos",
+        esely: 10 
+    },
+    {
+        nev: "Kulcsár Nándor",
+        esely: 10 
+    },
+    {
+        nev: "Balogh László Levente",
+        esely: 10 
+    },
+    {
+        nev: "Illés Csaba",
+        esely: 10 
+    },
+    {
+        nev: "Perecz István",
+        esely: 10 
+    },
+    {
+        nev: "Papp Eszter",
+        esely: 10 
+    },
+    {
+        nev: "Vitális Zsuzsanna",
+        esely: 10 
+    },
+    {
+        nev: "Süveges-Szabó Zita",
+        esely: 10 
+    },
+    {
+        nev: "Maróti Gábor",
+        esely: 10 
+    },
+    {
+        nev: "M. Tóth Milán",
+        esely: 10 
+    },
+
+]
---
+++ assets/js/selector.js
@@ -0,0 +1,11 @@
+var random = 0
+var osszesely =0
+    osztaly.forEach(element => {
+       osszesely+=element.esely 
+    });
+
+
+function sorsol(params) {
+    random = Math.floor(Math.random() * osszesely)
+    
+}
